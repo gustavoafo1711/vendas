ccb08513f2af055754e528a25872c2a18c6c7ee0
gustavo <gustavo@gustavo-Aspire-5741Z>
2022-11-21 17:46:42 -0300

    Pesquisa de Clientes por parâmetro

--- vendas/src/main/java/com/github/gustavoafo1711/vendas/domain/entity/Cliente.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/domain/entity/Cliente.java
@@ -24,6 +24,9 @@ public class Cliente {
 	
 	@Column(name = "nome", length = 100)
 	private String nome;
+	
+	@Column(name = "cpf", length= 11)
+	private String cpf;
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
@@ -63,6 +66,14 @@ public class Cliente {
 
 	public void setPedidos(Set<Pedido> pedidos) {
 		this.pedidos = pedidos;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
 	}
 
 	@Override
