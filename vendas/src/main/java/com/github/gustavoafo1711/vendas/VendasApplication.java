8b55ca68287c035d0f591e8c74da777e54c6e8ae
gustavo <gustavo@gustavo-Aspire-5741Z>
2022-11-21 16:21:30 -0300

    Requisição POST e RequestBody

--- vendas/src/main/java/com/github/gustavoafo1711/vendas/VendasApplication.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/VendasApplication.java
@@ -1,25 +1,10 @@
 package com.github.gustavoafo1711.vendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.github.gustavoafo1711.vendas.domain.entity.Cliente;
-import com.github.gustavoafo1711.vendas.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
-
-	@Bean
-	public CommandLineRunner clRunner(@Autowired Clientes clientes) {
-		return args -> {
-			Cliente c = new Cliente(null, "Galadriel");
-			clientes.save(c);
-		};
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
--- vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/controller/ClienteControler.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/controller/ClienteControler.java
@@ -2,11 +2,12 @@ package com.github.gustavoafo1711.vendas.rest.controller;
 
 import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.gustavoafo1711.vendas.domain.entity.Cliente;
@@ -31,6 +32,13 @@ public class ClienteControler {
 		}
 		return ResponseEntity.notFound().build();
 		
+	}
+	
+	@PostMapping("/api/clientes")
+	@ResponseBody
+	public ResponseEntity save(@RequestBody Cliente cliente) {
+		Cliente clienteSalvo = clientes.save(cliente);
+		return ResponseEntity.ok(clienteSalvo);
 	}
 
 }
