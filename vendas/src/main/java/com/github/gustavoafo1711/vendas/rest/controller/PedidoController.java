b46e6e874a3c9210883d0f34838584972bc18ba7
gustavo <gustavoafo@hotmail.com>
2022-11-28 15:53:57 -0300

    Validando  o PedidoDTO

--- vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/controller/PedidoController.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/controller/PedidoController.java
@@ -26,6 +26,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 
@@ -41,7 +43,7 @@ public class PedidoController {
 	
 	@PostMapping
 	@ResponseStatus(CREATED)
-	public Integer save(@RequestBody PedidoDTO dto) {
+	public Integer save(@RequestBody @Valid PedidoDTO dto) {
 		Pedido pedido = service.salvar(dto);
 		return pedido.getId();
 	}
--- vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/dto/PedidoDTO.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/rest/dto/PedidoDTO.java
@@ -3,6 +3,8 @@ package com.github.gustavoafo1711.vendas.rest.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoDTO {
-	
+	@NotNull(message = "Informe o código do cliente.")
 	private Integer cliente;
+	
+	@NotNull(message = "Campo total do pedido é obrigatório.")
 	private BigDecimal total;
+	
 	private List<ItemPedidoDTO> items;
 
 }
