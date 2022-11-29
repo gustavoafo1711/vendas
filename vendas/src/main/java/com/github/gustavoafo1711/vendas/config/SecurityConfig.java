c6430a19f89a064528097adc2fc1a01fafbef43c
gustavo <gustavoafo@hotmail.com>
2022-11-29 20:06:23 -0300

    Configurando a autorização de URLs

--- vendas/src/main/java/com/github/gustavoafo1711/vendas/config/SecurityConfig.java
+++ vendas/src/main/java/com/github/gustavoafo1711/vendas/config/SecurityConfig.java
@@ -21,14 +21,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.passwordEncoder(passwordEncoder())
-			.withUser("Rhaenyra")
+			.withUser("Rhaenyra Targaryen")
 			.password(passwordEncoder().encode("123"))
 			.roles("USER");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+			.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/api/clientes/**")
+					.hasRole("USER")
+			.and()
+				.formLogin();
 	}
 }
